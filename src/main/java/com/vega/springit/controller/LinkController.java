ae8a86998198b96690022ad524b9cbac79ab49d6
912601 <mauro.avon@insiel.it>
2022-06-13 18:13:59 +0200

    layouts

--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -2,50 +2,23 @@ package com.vega.springit.controller;
 
 import com.vega.springit.domain.Link;
 import com.vega.springit.repository.LinkRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/links")
+@Controller
 public class LinkController {
 
-    private LinkRepository linkRepository;
 
-    public LinkController(LinkRepository linkRepository) {
-        this.linkRepository = linkRepository;
-
+    @GetMapping("/foo")
+    public String foo(Model model) {
+        model.addAttribute("pageTitle", "This page is FOO!");
+        return "foo";
     }
 
 
-
-    // list
-    @GetMapping("/")
-    public List<Link> list() {
-        return linkRepository.findAll();
-    }
-
-    // CRUD
-    @PostMapping("/create")
-    public Link create(@ModelAttribute Link link) {
-        return linkRepository.save(link);
-    }
-
-    @GetMapping("/{id}")
-    public Optional<Link> read(@PathVariable Long id) {
-        return linkRepository.findById(id);
-    }
-
-    @PutMapping("/{id}")
-    public Link update(@PathVariable Long id, @ModelAttribute Link link) {
-        return linkRepository.save(link);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        linkRepository.deleteById(id);
-
-    }
 
 }
