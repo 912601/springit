218de2b39e4d9511ff708f17da8c6ac81cfadf45
912601 <mauro.avon@insiel.it>
2022-06-17 16:23:45 +0200

    view layer

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.ocpsoft.prettytime.PrettyTime;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class SpringitApplication {
@@ -24,5 +25,9 @@ public class SpringitApplication {
 
     }
 
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
 
 }
